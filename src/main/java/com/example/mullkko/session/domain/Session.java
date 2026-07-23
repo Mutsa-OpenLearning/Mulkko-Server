3d7310f585de6aa121b9a975a0f43e38c2b242a1
KangSangJun <befur1@g.hongik.ac.kr>
2026-07-23 12:27:20 +0900

    [feat] 세션/유저 도메인 수정

--- src/main/java/com/example/mullkko/session/domain/Session.java
+++ src/main/java/com/example/mullkko/session/domain/Session.java
@@ -1,8 +1,10 @@
 package com.example.mullkko.session.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Session {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
