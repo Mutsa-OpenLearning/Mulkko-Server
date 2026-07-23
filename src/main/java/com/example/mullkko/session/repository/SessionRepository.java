0231354a2be85ebb8c2699aee1b06711f13b3fa8
KangSangJun <befur1@g.hongik.ac.kr>
2026-07-23 12:27:25 +0900

    [feat] 세션/유저 레포 수정

--- src/main/java/com/example/mullkko/session/repository/SessionRepository.java
+++ src/main/java/com/example/mullkko/session/repository/SessionRepository.java
@@ -3,6 +3,8 @@ package com.example.mullkko.session.repository;
 import com.example.mullkko.session.domain.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    Session findBySessionCode(String sessionCode);
+    Optional<Session> findBySessionCode(String sessionCode);
 }
