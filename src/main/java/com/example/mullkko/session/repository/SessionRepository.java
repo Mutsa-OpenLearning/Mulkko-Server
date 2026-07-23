38e3e6078eddb5f6ad217ae177d7af5742ddb6dd
KangSangJun <befur1@g.hongik.ac.kr>
2026-07-23 13:07:16 +0900

    [feat] session 도메인/레포지토리 구현

--- src/main/java/com/example/mullkko/session/repository/SessionRepository.java
+++ src/main/java/com/example/mullkko/session/repository/SessionRepository.java
@@ -3,8 +3,11 @@ package com.example.mullkko.session.repository;
 import com.example.mullkko.session.domain.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
     Optional<Session> findBySessionCode(String sessionCode);
+    boolean existsBySessionCode(String sessionCode);
+    List<Session> findByHostId(Long hostId);
 }
